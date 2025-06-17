8d3d7cc4505e4df6c54ee1abb436c0966fa8b241
FacundoJarma <49127963@est.ort.edu.ar>
2025-06-17 09:03:34 -0300

    a

---
+++ Assets/CubesManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubesManager : MonoBehaviour
+{
+
+    public CuboScript[] cuboScripts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < cuboScripts.Length; i++) { 
+
+            Debug.Log(cuboScripts[i].esPersistente);
+        }
+    }
+
+}
